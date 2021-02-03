187856ac863fe6e05a65faafeb83b56aac0e7673
Ronald Yonggi <ronaldyonggi@gmail.com>
2021-02-03 09:03:41 -0500

    port reverted back

--- web.py
+++ web.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
- 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="localhost", port=8080, debug=True)
